3559fd8684c6204bb4b07ebf47e1976d25f14ea1
Erick Custodio <ecust@dhcp-v224-181.vp.reshsg.uci.edu>
2015-12-24 10:59:16 -0800

    Initial Commit

---
+++ VidCoach2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VidCoach2
+//
+//  Created by Erick Custodio on 12/24/15.
+//  Copyright © 2015 Erick Custodio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

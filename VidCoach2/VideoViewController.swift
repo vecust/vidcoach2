f58888f8a4ab093e5a84899563335c477db8a888
Erick Custodio <ecust@dhcp-v224-181.vp.reshsg.uci.edu>
2015-12-31 14:16:38 -0800

    Completed Prompt Settings

--- VidCoach2/VideoViewController.swift
+++ VidCoach2/VideoViewController.swift
@@ -17,11 +17,18 @@ class VideoViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var interviewQuestion: UILabel!
+    @IBOutlet weak var prePromptSwitch: UISwitch!
+    @IBOutlet weak var postPromptSwitch: UISwitch!
     var url4Player = [NSURL()]
     var playerItems = [AVPlayerItem]()
     var interview = String()
     var question = String()
     var mode = String()
+    var prePromptON = Bool()
+    var postPromptON = Bool()
+    var settingDict = NSMutableDictionary()
+    var settingPath = String()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +37,25 @@ class VideoViewController: UIViewController {
         image.image = UIImage(named: interview)
         title = question
         
-        // Load video
+        //check prompt settings
+        settingPath = NSBundle.mainBundle().pathForResource("PromptSettings", ofType: "plist")!
+        settingDict = NSMutableDictionary(contentsOfFile: settingPath)!
+        
+        prePromptON = (settingDict.objectForKey("PrePrompt") as? Bool)!
+        postPromptON = (settingDict.objectForKey("PostPrompt") as? Bool)!
+        
+        if prePromptON {
+            self.prePromptSwitch.setOn(true, animated: false)
+        } else {
+            self.prePromptSwitch.setOn(false, animated: false)
+        }
+        
+        if postPromptON {
+            self.postPromptSwitch.setOn(true, animated: false)
+        } else {
+            self.postPromptSwitch.setOn(false, animated: false)
+        }
+
         
     }
 
@@ -42,18 +67,39 @@ class VideoViewController: UIViewController {
     // MARK: Actions
     
     @IBAction func watchModel(sender: UIButton) {
-        print("Tapped Watch Model")
+//        print("Tapped Watch Model")
         mode = "Watch Model"
     }
     
     @IBAction func practice(sender: UIButton) {
-        print("Tapped Practice")
+//        print("Tapped Practice")
         mode = "Practice"
     }
     
     @IBAction func watchPractice(sender: UIButton) {
-        print("Tapped Watch Practice")
+//        print("Tapped Watch Practice")
         mode = "Watch Practice"
+    }
+    
+    @IBAction func prePromptSetting(sender: UISwitch) {
+//        print("Switched PrePrompt Setting")
+        if sender.on {
+            prePromptON = true
+            settingDict.setValue(true, forKey: "PrePrompt")
+        } else {
+            prePromptON = false
+            settingDict.setValue(false, forKey: "PrePrompt")
+        }
+    }
+    
+    @IBAction func postPromptSetting(sender: UISwitch) {
+        if sender.on {
+            postPromptON = true
+            settingDict.setValue(true, forKey: "PostPrompt")
+        } else {
+            postPromptON = false
+            settingDict.setValue(false, forKey: "PostPrompt")
+        }
     }
     
     // MARK: Navigation
@@ -61,6 +107,9 @@ class VideoViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let destination = segue.destinationViewController as! AVQueuePlayerViewController
         destination.action = mode
+        
+        //write prompt settings
+        settingDict.writeToFile(settingPath, atomically: false)
         
         if segue.identifier == "watchModel" {
             destination.interview = interview
@@ -74,6 +123,11 @@ class VideoViewController: UIViewController {
 //            destination.player = AVPlayer(URL: url4Player[3])
 //            destination.player?.play()
         }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        //write prompt settings
+        settingDict.writeToFile(settingPath, atomically: false)
     }
     
     
